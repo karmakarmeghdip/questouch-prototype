5f74e5915d818a2146dea79aea3008864fb8b640
karmakarmeghdip <34504765+karmakarmeghdip@users.noreply.github.com>
2022-10-29 21:32:19 +0530

    kill me

--- taglist.php
+++ taglist.php
@@ -2,7 +2,7 @@
 $file = fopen("tags.csv", "r");
 $tags=array("CU", "Physics");
 while (($data = fgetcsv($file)) !== false) {
-    $tags=array_merge(array_splice($i, 1), $tags);
+    $tags=array_merge(array_splice($data, 1), $tags);
 }
 
 echo var_dump($tags);
