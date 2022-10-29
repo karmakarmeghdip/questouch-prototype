47933236ede435f37fd837965d191a3af2b3f09e
karmakarmeghdip <34504765+karmakarmeghdip@users.noreply.github.com>
2022-10-29 20:07:55 +0530

    missed the semicolons

--- index.php
+++ index.php
@@ -49,12 +49,12 @@
 
               $file = fopen("tags.csv", "r");
 
-              //while (($data = fgetcsv($file)) !== false) {
-              //  foreach ($data as $i) {
-              //    echo htmlspecialchars($i)." "
-              //  }
-              //  echo "<br>"
-              //}
+              while (($data = fgetcsv($file)) !== false) {
+                foreach ($data as $i) {
+                  echo htmlspecialchars($i)." ";
+                }
+                echo "<br>";
+              }
               ?>
             </div>
 
