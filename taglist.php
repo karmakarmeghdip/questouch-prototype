ac233e72808429a5269bd927bf48b3b4b1d26ffa
karmakarmeghdip <34504765+karmakarmeghdip@users.noreply.github.com>
2022-10-29 21:28:26 +0530

    bugfixes

--- taglist.php
+++ taglist.php
@@ -1,6 +1,6 @@
 <?php
 $file = fopen("tags.csv", "r");
-$tags=array();
+$tags=array("CU", "Physics");
 while (($data = fgetcsv($file)) !== false) {
     foreach ($data as $i) {
         $tags=array_merge(array_splice($i, 1), $tags);
