1c7b95d6d90beafdadc6373919425e605edba46e
karmakarmeghdip <34504765+karmakarmeghdip@users.noreply.github.com>
2022-10-29 18:59:05 +0530

    Basic Frontend Implementation

---
+++ index.php
@@ -0,0 +1,51 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>QUESTOUCH-Question on your one touch</title>
+  <link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+<body>
+        <nav class="navbar background">
+            <ul class="nav-list">
+              <div class="logo">
+                <img src="assets/logo.jpg">
+              </div>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="http://wa.me/919475218087">Contact DTL</a></li>
+            </ul>
+
+           <!-- <div class="rightNav">
+                <input type="text" name="search" id="search">
+                <button class="btn btn-sm">Search</button>
+            </div> -->
+        </nav>
+
+        <section class="background firstSection">
+          <div class="box-main">
+              <div class="firstHalf">
+                  <p class="text-big">Question on your one touch.....</p>
+                  <p class="text-small">This is a preliminary step to provied previous years questions to KGEC students. But for now it is just in a trail phase . </p>
+                    <div class="button">
+                        <button class="btn"><a href="https://www.kgec.edu.in/">Learn More</a></button>
+                    </div>
+
+              <div class="secondHalf">
+                  <img src="assets/kgec.jpg" alt="kgec image">
+              </div>
+
+              <div class="SearchBar">
+                <input type="text" placeholder="Enter the tag.." name="search" id="search">
+                <button class="btn btn-sm">Search</button>
+            </div>
+
+            <div class="poster">
+              <img src="assets/poster.jpg" alt="poster">
+            </div>
+          </div>
+        </section>
+
+</body>
+</html>
