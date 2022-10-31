5cf743c6b7a1a7403c6e24542a8c1b0e3901c5b9
karmakarmeghdip <34504765+karmakarmeghdip@users.noreply.github.com>
2022-10-31 22:33:15 +0530

    less gooo

--- home.php
+++ home.php
@@ -76,9 +76,21 @@
                     <input type="submit" value="Search">
                     </form>
 
-            <div class="poster">
+            <!--<div class="poster">
               <img src="assets/poster.jpg" alt="poster">
             </div>
+                  -->
+
+                  <div class="px-4 pt-5 my-5 text-center border-bottom">
+    <h1 class="display-4 fw-bold">Centered screenshot</h1>
+      
+ 
+    <div class="overflow-hidden" style="max-height: 30vh;">
+      <div class="container px-5">
+        <img src="assets/poster.jpg" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+      </div>
+    </div>
+  </div>
           </div>
         </main>
 
