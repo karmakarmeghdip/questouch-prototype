bb77594f9a160e3089836b70b5e6e5e3297a48d3
karmakarmeghdip <34504765+karmakarmeghdip@users.noreply.github.com>
2022-10-31 22:38:37 +0530

    cleanup

--- home.php
+++ home.php
@@ -82,9 +82,6 @@
                   -->
 
                   <div class="px-4 pt-5 my-5 text-center border-bottom">
-    <h1 class="display-4 fw-bold">Centered screenshot</h1>
-      
- 
     <div class="overflow-hidden">
       <div class="container px-5">
         <img src="assets/poster.jpg" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="1280" loading="lazy">
